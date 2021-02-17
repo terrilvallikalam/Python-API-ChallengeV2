1ebc23359618e9b53d14278ad53efc0e5158fddd
Terril Vallikalam <terrilvallikalam@Terrils-MacBook-Pro.local>
2021-02-16 19:20:34 -0600

    vacation

--- VacationPy/api_keys.py
+++ VacationPy/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE!"
+weather_api_key = "719120c07f94f126ea170eada65a3106"
 
 # Google API Key
-g_key = "YOUR KEY HERE!"
+g_key = "AIzaSyA0kLKThmuRd2yEOh8axX7reLVGY2htbrE"
